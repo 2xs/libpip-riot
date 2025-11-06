f3bb712dad5b8b4f23892a37d43b9ac4604a6630
Gregory Guche <gregory.guche@univ-lille.fr>
2025-11-06 15:49:23 +0100

    Added CRT0 generic and PIP specific parts for refactoring.

---
+++ crt0_ctx.h
@@ -0,0 +1,36 @@
+#ifndef __FAE_CRT0_CONTEXT_H__
+#define __FAE_CRT0_CONTEXT_H__
+
+/**
+ * @brief Data structure that describes the memory layout
+ * required by the CRT0 to execute the relocatable binary
+ */
+typedef struct crt0_ctx_s {
+    /*
+     * Start address of the binary in the NVM
+     */
+    void *bin_base;
+    /**
+     * Start address of the available free RAM
+     */
+    void *ram_start;
+    /**
+     * End address of the available free RAM
+     */
+    void *ram_end;
+    /**
+     * Start address of the free NVM
+     */
+    void *nvm_start;
+    /**
+     * End address of the free NVM
+     */
+    void *nvm_end;
+    /**
+     * Arguments passed to the relocatable binary.
+     * This is up to caller and callee to agree on a common arguments type.
+     */
+    void *argv;
+} crt0_ctx_t;
+
+#endif /*  __FAE_CRT0_CONTEXT_H__ */
