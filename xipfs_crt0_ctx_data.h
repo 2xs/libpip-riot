b3e07d9b674659b29fc09262a92deaff9d153459
Gregory Guche <gregory.guche@univ-lille.fr>
2025-11-07 14:02:52 +0100

    Added XIPFS specific data for refactoring.

---
+++ xipfs_crt0_ctx_data.h
@@ -0,0 +1,48 @@
+#ifndef __XIPFS_CRT0_CONTEXT_DATA_H__
+#define __XIPFS_CRT0_CONTEXT_DATA_H__
+
+/**
+ * @brief XIPFS Max Command Line arguments count.
+ * @warning MUST REMAIN SYNCHRONIZED with xipfs's file definition.
+ */
+#define XIPFS_EXEC_ARGC_MAX (64)
+
+typedef struct xipfs_crt0_ctx_data_s {
+    /*
+     * Start address of the file in NVM,
+     * which is the text segment of the xipfs file.
+     */
+    void *file_base;
+    /*
+     * true if the context is executed in user mode with configured MPU regions,
+     * false otherwise
+     */
+    unsigned char is_safe_call;
+    /*
+     * Number of arguments passed to the relocatable binary.
+     */
+    int argc;
+    /*
+     * Arguments passed to the relocatable binary.
+     */
+    char *argv[XIPFS_EXEC_ARGC_MAX];
+    /*
+     * Table of function pointers for functions used by CRT0 and/or stdriot.
+     */
+    const void **syscall_table;
+    /**
+     * When using xipfs_file_safe_exec, syscalls results will be written here.
+     */
+    int syscall_result;
+    /*
+     * RIOT's Global Offset Table
+     */
+    const void *former_got;
+    /*
+     * Relocatable binary's current got
+     */
+    const void *current_got;
+
+} xipfs_crt0_ctx_data_t;
+
+#endif /* __XIPFS_CRT0_CONTEXT_DATA_H__ */
